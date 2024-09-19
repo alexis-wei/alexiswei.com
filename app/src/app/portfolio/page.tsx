29aa2ed5d7567337639d4715e4629a3ac0d8e99f
Alexis Wei <qq_wei369@hotmail.com>
2024-09-19 09:42:56 -0400

    rename pages

---
+++ app/src/app/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>Welcome to my portfolio page!</p>
+    </div>
+  );
+}
