17ed57a738b273653f290bb29c0b4c4c43d84392
Alexis Wei <qq_wei369@hotmail.com>
2024-09-20 01:02:07 -0400

    remove

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomePage from "@/components/Home";
+import HomePage from "@/components/home";
 import Welcome from "@/components/saturn/Welcome";
 
 export default function Home() {
