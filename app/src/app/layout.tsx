22be03f001072cc257b14b30018019408ad4cef5
Alexis Wei <qq_wei369@hotmail.com>
2024-10-25 00:47:19 -0400

    add new favicon

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -24,6 +24,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link
+        rel="icon"
+        type="image/png"
+        href="/favicon-48x48.png"
+        sizes="48x48"
+      />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <meta name="apple-mobile-web-app-title" content="alexiswei" />
+      <link rel="manifest" href="/site.webmanifest" />
       <body>
         <MantineProvider theme={theme}>{children}</MantineProvider>
       </body>
