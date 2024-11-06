07f4a0634f03432270d5dcaf9bd6ef39feee292c
Alexis Wei <qq_wei369@hotmail.com>
2024-11-05 21:06:12 -0500

    remove unused interface

--- portfolio/src/components/portfolio/Main.tsx
+++ portfolio/src/components/portfolio/Main.tsx
@@ -3,9 +3,7 @@
 import { FC } from "react";
 import TheDrop from "./TheDrop";
 
-interface MainProps {}
-
-const Main: FC<MainProps> = (): JSX.Element => {
+const Main: FC = (): JSX.Element => {
   return (
     <div className="flex min-h-screen flex-col items-center">
       <div className="flex w-full max-w-[1600px] items-center">
