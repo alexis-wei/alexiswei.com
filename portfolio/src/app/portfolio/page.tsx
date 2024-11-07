2881fa84466a5df08fe2368bea6ccd9b89b2f3f5
Alexis Wei <qq_wei369@hotmail.com>
2024-11-07 00:17:46 -0500

    add suspense as per react rules

--- portfolio/src/app/portfolio/page.tsx
+++ portfolio/src/app/portfolio/page.tsx
@@ -2,12 +2,22 @@
 
 import Main from "@/components/portfolio/Main";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
-const Portfolio = () => {
+function MainDiv() {
   const searchParams = useSearchParams();
+
   const page = Number(searchParams?.get("page")) || 1;
 
   return <Main page={page} />;
+}
+
+const Portfolio = () => {
+  return (
+    <Suspense>
+      <MainDiv />
+    </Suspense>
+  );
 };
 
 export default Portfolio;
