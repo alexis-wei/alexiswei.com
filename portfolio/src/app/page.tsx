baa249bd6b322cfdbc7b5fa7383edba8cd3d79a5
Alexis Wei <qq_wei369@hotmail.com>
2024-11-07 11:11:04 -0500

    Detect if already on page (#13)

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -1,11 +1,5 @@
 import Main from "@/components/home/Main";
-import Welcome from "@/components/home/Welcome";
 
 export default function Home() {
-  return (
-    <div className="flex h-dvh w-dvw items-center justify-center">
-      <Main />
-      <Welcome />
-    </div>
-  );
+  return <Main />;
 }
