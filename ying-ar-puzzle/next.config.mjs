f6192380e174f7ca4dfeae983f43cfdebad76661
Alexis Wei <qq_wei369@hotmail.com>
2024-11-29 16:42:20 -0500

    remove unplug icons stuff and use dynamic imports

--- ying-ar-puzzle/next.config.mjs
+++ ying-ar-puzzle/next.config.mjs
@@ -1,8 +1,3 @@
-// next.config.mjs
-import Icons from "unplugin-icons/webpack";
-import IconsResolver from "unplugin-icons/resolver";
-import AutoImport from "unplugin-auto-import/webpack";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -21,24 +16,6 @@ const nextConfig = {
       ...config.resolve.fallback,
       fs: false,
     };
-    config.plugins.push(
-      AutoImport({
-        resolvers: [
-          IconsResolver({
-            prefix: "Icon",
-            extension: "jsx",
-          }),
-        ],
-        dts: true,
-        eslintrc: {
-          enabled: true,
-        },
-      }),
-      Icons({
-        compiler: "jsx",
-        jsx: "react",
-      }),
-    );
 
     return config;
   },
--- ying-ar-puzzle/src/components/index.tsx
+++ ying-ar-puzzle/src/components/index.tsx
@@ -1,4 +1,8 @@
-import ARScene from "./ARScene.jsx";
+import dynamic from "next/dynamic";
+
+const ARScene = dynamic(() => import("./ARScene.jsx"), {
+  ssr: false,
+});
 
 export default function ARMap() {
   return <ARScene />;
