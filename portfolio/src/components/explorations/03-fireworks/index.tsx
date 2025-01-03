d08ac002160d4e5f901dcd079cf3a8d02ac45872
Alexis Wei <qq_wei369@hotmail.com>
2025-01-03 12:17:29 -0500

    photo frame size increase

--- portfolio/src/components/explorations/03-fireworks/index.tsx
+++ portfolio/src/components/explorations/03-fireworks/index.tsx
@@ -1,7 +1,22 @@
 "use client";
 
+import { motion, useScroll, useTransform } from "motion/react";
+
 import Scene2 from "./Scene2";
 
 export default function Fireworks() {
-  return <Scene2 />;
+  const { scrollYProgress } = useScroll();
+  const scale = useTransform(scrollYProgress, [0, 1], [0.5, 1.2]);
+
+  return (
+    <div className="min-h-[200vh] w-screen bg-black">
+      <div className="fixed inset-0 grid place-items-center">
+        <motion.img
+          src="/explorations/03-fireworks/vintage-picture-frame.png"
+          alt="vintage picture frame"
+          style={{ scale }}
+        />
+      </div>
+    </div>
+  );
 }
