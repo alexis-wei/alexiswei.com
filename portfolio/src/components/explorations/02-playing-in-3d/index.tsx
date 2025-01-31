0b304b72ec417dc3615611a43f8d55047812e61a
Alexis Wei <qq_wei369@hotmail.com>
2025-01-31 18:13:38 -0500

    update exploration page description and format

--- portfolio/src/components/explorations/02-playing-in-3d/index.tsx
+++ portfolio/src/components/explorations/02-playing-in-3d/index.tsx
@@ -10,11 +10,10 @@ const Main = () => {
         <h2 className="leading-none tracking-tighter">02-playing in 3d</h2>
         <p className="font-serif text-xs font-bold text-stone-600">11/21/24</p>
         <p className="max-w-[400px] text-sm tracking-tighter">
-          from setting up a scene in 3js to how to use shaders. a wide range of
-          topics with so many variables to think through. trying to explore how
-          to make realistic effects through combining tools and available
-          libraries such as react 3 fiber and drei, trying to understand how
-          each parameter can change the effects
+          setting up a scene in 3js, playing with different materials, lighting
+          and the way 3d shape can impact and effect the outcome of the visuals.
+          learning available libraries such as react 3 fiber and drei, trying to
+          understand how each parameter can change the effects
         </p>
       </div>
       <div className="flex flex-col gap-6">
