68c76611fdfb8646c80d9b1ee1b6ae73b736d340
Alexis Wei <qq_wei369@hotmail.com>
2025-02-05 11:48:30 -0500

    add shader intro, test out cloudflare on prod

---
+++ portfolio/src/components/explorations/05-shader-animations-01/index.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import PhotoCanvasOne from "./PhotoCanvasOne";
+import { ExplorationIntro } from "../ui";
+
+export default function ShaderAnimations() {
+  const introDescription = `vertex and color changes as a result of shader value tracking. 
+  playing with vacation photos, variables such as time, position to create fun effects`;
+  return (
+    <div className="flex min-h-dvh w-dvw flex-col items-center justify-start gap-8 p-5 pb-32">
+      <ExplorationIntro
+        name="05-shader-animations"
+        description={introDescription}
+        date="02/05/25"
+      />
+      <div className="flex flex-col gap-6">
+        <PhotoCanvasOne />
+      </div>
+    </div>
+  );
+}
